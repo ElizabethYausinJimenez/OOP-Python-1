24235af3af6bf9832d4600796d32bfd96568958c
Elizabeth Yausin Jimenez <168606283+ElizabethYausinJimenez@users.noreply.github.com>
2024-06-09 19:03:01 -0400

    Update maintarjeta.py

--- maintarjeta.py
+++ maintarjeta.py
@@ -1,8 +1,8 @@
 from tarjetacredito import TarjetaCredito
 
-tarjeta1 = TarjetaCredito()
-tarjeta2 = TarjetaCredito()
-tarjeta3 = TarjetaCredito()
+tarjeta1 = TarjetaCredito(0, 1000, 0.02)
+tarjeta2 = TarjetaCredito(0, 1000, 0.02)
+tarjeta3 = TarjetaCredito(0, 1000, 0.02)
 
 print(tarjeta1.compra(500),tarjeta1.compra(200),tarjeta1.pago(100))
 tarjeta1.cobrar_interes()
